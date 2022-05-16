b7ef3701c144e172dbbb0c9f2a11737c56bd1437
James Yim <jnyim@ucsc.edu>
2022-05-15 23:34:14 -0700

    Finished AutoClicker

--- AutoClicker/Clicker.py
+++ AutoClicker/Clicker.py
@@ -1,5 +1,21 @@
+import keyboard
+import mouse
+#import pyautogui
+
+#
+# import pyscreenshot as ImageGrab
+
 def main():
     print("poop")
+    isRunning = False
+    while True:
+        if (keyboard.is_pressed("s")):
+            isRunning = True
+        while isRunning:
+            if (keyboard.is_pressed("a")):
+                isRunning = False
+            mouse.click("left") 
 
 if __name__ == "__main__":
      main()
+
