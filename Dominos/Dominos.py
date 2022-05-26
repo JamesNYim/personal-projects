f096d89d81448f73e04b546dfd705595d3a60fb1
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2022-05-25 19:11:23 -0700

    Finished Inital Dominos game.

--- Dominos/Dominos.py
+++ Dominos/Dominos.py
@@ -1,4 +1,39 @@
 import random
+def drawDomino(dominoSet, playerSet):
+        randIndex = random.randint(0, len(dominoSet) - 1)
+        playerSet.append(dominoSet[randIndex])
+        dominoSet.remove(dominoSet[randIndex])
+
+def playingDomino(playerSet, dominoBoard):
+    dominoPlayed = False
+    for playDomino in playerSet:
+            if len(dominoBoard) == 0:
+                dominoBoard.append(playDomino)
+                playerSet.remove(playDomino)
+            else:
+                if dominoBoard[0][0] == playDomino[1]:
+                    dominoBoard.insert(0, [playDomino[0], playDomino[1]])
+                    playerSet.remove(playDomino)
+                    dominoPlayed = True
+                    break
+                elif dominoBoard[0][0] == playDomino[0]:
+                    dominoBoard.insert(0, [playDomino[1], playDomino[0]])
+                    playerSet.remove(playDomino)
+                    dominoPlayed = True
+                    break
+                elif dominoBoard[len(dominoBoard) - 1][1] == playDomino[0]:
+                    dominoBoard.append([playDomino[0], playDomino[1]])
+                    playerSet.remove(playDomino)
+                    dominoPlayed = True
+                    break
+                elif dominoBoard[len(dominoBoard) - 1][1] == playDomino[1]:
+                    dominoBoard.append([playDomino[1], playDomino[0]])
+                    playerSet.remove(playDomino)
+                    dominoPlayed = True
+                    break
+    return dominoPlayed
+    
+
 
 def main():
     #Making the set of dominos
@@ -7,20 +42,55 @@ def main():
     #Getting the domino set for the first player
     playerOneSet = []
     while len(playerOneSet) < 7:
-        randIndex = random.randint(0, len(dominoSet) - 1)
-        playerOneSet.append(dominoSet[randIndex])
-        dominoSet.remove(dominoSet[randIndex])
+        drawDomino(dominoSet, playerOneSet)
    
     #Getting the domino set for the second player
     playerTwoSet = []
     while len(playerTwoSet) < 7:
-        randIndex = random.randint(0, len(dominoSet) -1)
-        playerTwoSet.append(dominoSet[randIndex])
-        dominoSet.remove(dominoSet[randIndex])
+        drawDomino(dominoSet, playerTwoSet)
 
-    print(playerOneSet)  
-    print("\n")  
-    print(playerTwoSet)
+    #The Game of dominos
+    playerOnePoints = 0
+    playerTwoPoints = 0
+    playerOneWin = False
+    playerTwoWin = False
+    dominoPlayBoard = []
+    while (not playerOneWin or not playerTwoWin):
+
+        #Player One Turn
+        dominoPlayed = playingDomino(playerOneSet, dominoPlayBoard)
+        if not dominoPlayed:
+            drawDomino(dominoSet, playerOneSet)
+            playingDomino(playerOneSet, dominoPlayBoard)
+
+        #Player Two Turn
+        dominoPlayed = playingDomino(playerTwoSet, dominoPlayBoard)
+        if not dominoPlayed:
+            drawDomino(dominoSet, playerTwoSet)
+            playingDomino(playerTwoSet, dominoPlayBoard)
+        
+        #Checking for a winner
+        if (len(playerOneSet) == 0):
+            playerOneWin = True
+            break
+
+        if (len(playerTwoSet) == 0):
+            playerTwoWin = True
+            break
+
+    #Printing out win results
+    if playerOneWin:
+        print("PlayerOne has won")
+    if playerTwoWin:
+        print("Player Two has won")
+    
+    print(dominoPlayBoard)
+        
+
+
+        
+
+
 
         
 
