fe78e2e6e7323f4fc1869fcde5896c2516bfe813
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2023-11-06 22:00:23 -0800

    Able to read to file

---
+++ FileWriter/Main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	ifstream in;
+	ofstream out;
+	string line;
+	int lineCount = 1;
+
+	// Checking for proper input
+	if (argc != 3)
+	{
+		cerr << "Usage: " << argv[0] << "<input file> <output file>" << endl;
+		return EXIT_FAILURE;
+	}
+
+	// Opening the input file for reading
+	in.open(argv[1]);
+	if (!in.is_open())
+	{
+		cerr << "Unable to open file " << argv[1] << " for reading" << endl;
+		return EXIT_FAILURE;
+	}
+
+	// Openiing the output file for writing
+	out.open(argv[2]);
+	if (!out.is_open())
+	{
+		cerr << "Unable to open file " << argv[2] << " for writing" << endl;
+		return EXIT_FAILURE;
+	}
+
+	// Reading from in file
+	string textBuffer;
+	while (getline(in, textBuffer))
+	{
+		cout << "\n" << textBuffer;
+	}
+
+	// Closing Files
+	in.close();
+	out.close();
+
+	return 0;
+}
